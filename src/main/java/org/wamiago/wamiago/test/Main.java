7cab8ee06abf3bb2c33aec5b415e76411c902622
Malek Bsaissa <91043333+AtfastrSlushyMaker@users.noreply.github.com>
2025-02-10 00:11:01 +0100

    crud bicycles

--- src/main/java/org/wamiago/wamiago/test/Main.java
+++ src/main/java/org/wamiago/wamiago/test/Main.java
@@ -1,10 +1,28 @@
 package org.wamiago.wamiago.test;
 
 import org.wamiago.wamiago.entities.Bicycle;
+import org.wamiago.wamiago.services.BicycleService;
 import org.wamiago.wamiago.utils.DataBase;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         DataBase.getInstance();
-    }
+        BicycleService bicycleService = new BicycleService();
+        Bicycle bicycle = new Bicycle();
+        bicycle.getStation().setId(1);
+        try {
+            bicycleService.create(bicycle);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+        List<Bicycle> bicycles = bicycleService.read();
+        for (Bicycle b : bicycles) {
+            System.out.println(b);}
+        }
+        catch (Exception e){
+            e.printStackTrace();}
+}
 }
