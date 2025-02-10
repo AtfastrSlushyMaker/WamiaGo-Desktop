7f3fde1e4a4adf925ca5d12cad8efce3ec2d0374
islem <117855994+islem201@users.noreply.github.com>
2025-02-10 17:09:38 +0100

    crud

--- src/main/java/org/wamiago/wamiago/test/Main.java
+++ src/main/java/org/wamiago/wamiago/test/Main.java
@@ -1,9 +1,50 @@
 package org.wamiago.wamiago.test;
 
+import org.wamiago.wamiago.entities.Booking;
+import org.wamiago.wamiago.entities.Trip;
+import org.wamiago.wamiago.services.BookingService;
+import org.wamiago.wamiago.services.TripService;
 import org.wamiago.wamiago.utils.DataBase;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         DataBase.getInstance();
+
+        TripService tripService = new TripService();
+        BookingService bookingService = new BookingService();
+
+        try {
+//            Trip trip = new Trip(0, "Tunis", "Sousse", new Date(), 50, 150.0, 2, 1);
+//            tripService.create(trip);
+
+
+            List<Trip> trips = tripService.read();
+            for (Trip t : trips) {
+                System.out.println(t.getIdTrip() + " " + t.getDepartureCity() + " " + t.getArrivalCity());
+            }
+
+
+//            Booking booking = new Booking(28, 1, 1, 2, "Confirmed");
+//            bookingService.create(booking);
+
+
+            List<Booking> bookings = bookingService.read();
+            for (Booking b : bookings) {
+                System.out.println(b.getIdBooking() + " " + b.getIdTrip() + " " + b.getIdPassenger() + " " + b.getReservedSeats() + " " + b.getStatus());
+            }
+
+
+            Booking updatedBooking = new Booking(3, 1, 1, 5, Booking.Status.Canceled);
+            bookingService.update(updatedBooking);
+
+//            bookingService.delete(2);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
