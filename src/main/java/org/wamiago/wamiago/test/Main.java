b924e4f1b0f2a8ca15df2447033b0c2dc671fa94
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2025-02-12 02:18:10 +0100

    Revert "Update Main.java"
    This reverts commit 7e4cb1a755223e1ebe13b0180741b3ed0f375ee9.

--- src/main/java/org/wamiago/wamiago/test/Main.java
+++ src/main/java/org/wamiago/wamiago/test/Main.java
@@ -10,7 +10,72 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        
+        UserService userService = new UserService();
+        DriverService driverService = new DriverService();
+
+        try {
+
+            Location location = new Location(1, "123 Main St", 36.8065f, 10.1815f);
+            User newUser = new User(1,
+                    "John Doe",
+                    "john@example.com",
+                    "15678",
+                    "securepassword",
+                    User.Role.CLIENT,
+                    location);
+            userService.addUser(newUser);
+
+            User otherUser = new User(2,
+                    "Ahmed Ali",
+                    "ahmed@example.com",
+                    "123456", "passs",
+                    User.Role.CLIENT, location);
+            userService.addUser(otherUser);
+
+            Driver driver = new Driver(
+                    1,
+                    otherUser.getId(),
+                    otherUser.getName(),
+                    otherUser.getEmail(),
+                    otherUser.getPhone(),
+                    otherUser.getPassword(),
+                    Driver.DriverRole.TAXI_DRIVER,
+                    otherUser.getLocation(),
+                    "111",
+                    1
+            );
+            driverService.addDriver(driver);
+
+            System.out.println("User and Driver added successfully!");
+
+            System.out.println("All users:");
+            userService.getAllUsers().forEach(System.out::println);
+
+            System.out.println("All drivers:");
+            driverService.getAllDrivers().forEach(System.out::println);
+
+            newUser.setName("John Updated");
+            userService.updateUser(newUser);
+            System.out.println("User updated!");
+
+            User retrievedUser = userService.getUserById(newUser.getId());
+            System.out.println("Retrieved User: " + retrievedUser);
+
+            driver.setName("Ahmed Updated");
+            driverService.updateDriver(driver);
+            System.out.println("Driver updated!");
+
+            Driver retrievedDriver = driverService.getDriverById(driver.getId());
+            System.out.println("Retrieved Driver: " + retrievedDriver);
+
+            userService.deleteUser(newUser.getId());
+            driverService.deleteDriver(driver.getId());
+            System.out.println("User and Driver deleted!");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
 
