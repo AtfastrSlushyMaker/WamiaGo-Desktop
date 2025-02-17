e11f188fb9681be141cc333212ac3f917ca2b23b
Malek Bsaissa <91043333+AtfastrSlushyMaker@users.noreply.github.com>
2025-02-17 23:19:01 +0100

    major changes and fixes (linked rides button w home button)

--- src/main/java/controllers/Home.java
+++ src/main/java/controllers/Home.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import controllers.dashboard.DashboardController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,10 +11,13 @@ public class Home extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Dashboard/dashboard.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/dashboard/dashboard.fxml"));
+        DashboardController dashboardController = new DashboardController();
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Dashboards");
+        primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
