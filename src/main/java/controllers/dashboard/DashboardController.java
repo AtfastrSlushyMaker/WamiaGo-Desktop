5eea4bf7709520634b49f532482ec2050e8b6c84
islem <117855994+islem201@users.noreply.github.com>
2025-02-19 14:33:26 +0100

    islem

--- src/main/java/controllers/dashboard/DashboardController.java
+++ src/main/java/controllers/dashboard/DashboardController.java
@@ -15,76 +15,76 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class DashboardController {
-        @FXML
-        private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-        @FXML
-        private URL location;
+    @FXML
+    private URL location;
 
-        @FXML
-        private Button bicycle_rent_button;
+    @FXML
+    private Button bicycle_rent_button;
 
-        @FXML
-        private Button booking_button;
+    @FXML
+    private Button booking_button;
 
-        @FXML
-        private Button carpool_join_button;
+    @FXML
+    private Button carpool_join_button;
 
-        @FXML
-        private Label date_label;
+    @FXML
+    private Label date_label;
 
-        @FXML
-        private Button history_button;
+    @FXML
+    private Button history_button;
 
-        @FXML
-        private Button home_button;
+    @FXML
+    private Button home_button;
 
-        @FXML
-        private Button logout_button;
+    @FXML
+    private Button logout_button;
 
-        @FXML
-        private Pane pane_11;
+    @FXML
+    private Pane pane_11;
 
-        @FXML
-        private Pane pane_111;
+    @FXML
+    private Pane pane_111;
 
-        @FXML
-        private Pane pane_1111;
+    @FXML
+    private Pane pane_1111;
 
-        @FXML
-        private Pane pane_112;
+    @FXML
+    private Pane pane_112;
 
-        @FXML
-        private Pane pane_1121;
+    @FXML
+    private Pane pane_1121;
 
-        @FXML
-        private Pane pane_131;
+    @FXML
+    private Pane pane_131;
 
-        @FXML
-        private Button profile_button;
+    @FXML
+    private Button profile_button;
 
-        @FXML
-        private Button reclamation_button;
+    @FXML
+    private Button reclamation_button;
 
-        @FXML
-        private Button relocation_reserve_button;
+    @FXML
+    private Button relocation_reserve_button;
 
-        @FXML
-        private Button rides_button;
+    @FXML
+    private Button rides_button;
 
-        @FXML
-        private HBox root;
+    @FXML
+    private HBox root;
 
-        @FXML
-        private AnchorPane side_ankerpane;
+    @FXML
+    private AnchorPane side_ankerpane;
 
-        @FXML
-        private Button taxi_request_button;
+    @FXML
+    private Button taxi_request_button;
 
-        @FXML
-        private Label user_name_label;
-    private void pageNavigation()
-    {
+    @FXML
+    private Label user_name_label;
+
+    private void pageNavigation() {
         home_button.setOnAction(event -> {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/dashboard/dashboard.fxml"));
@@ -96,6 +96,7 @@ public class DashboardController {
                 e.printStackTrace();
             }
         });
+
         rides_button.setOnAction(event -> {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/rides/rides.fxml"));
@@ -107,17 +108,26 @@ public class DashboardController {
                 e.printStackTrace();
             }
         });
+
+        carpool_join_button.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/trips/trip.fxml"));
+                Parent tripRoot = loader.load();
+                Scene tripScene = new Scene(tripRoot);
+                Stage stage = (Stage) carpool_join_button.getScene().getWindow();
+                stage.setScene(tripScene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
-        @FXML
-        void initialize() {
-            pageNavigation();
-        }
 
+    @FXML
+    void initialize() {
+        pageNavigation();
+    }
 
-        public void setUser_name_label(String user_name) {
-            user_name_label.setText(user_name);
-        }
-
-
-
+    public void setUser_name_label(String user_name) {
+        user_name_label.setText(user_name);
+    }
 }
--- src/main/java/controllers/rides/RidesController.java
+++ src/main/java/controllers/rides/RidesController.java
@@ -1,100 +1,115 @@
 package controllers.rides;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+    import javafx.fxml.FXML;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.Parent;
+    import javafx.scene.Scene;
+    import javafx.scene.control.Button;
+    import javafx.scene.layout.AnchorPane;
+    import javafx.scene.layout.HBox;
+    import javafx.scene.layout.Pane;
+    import javafx.stage.Stage;
 
-public class RidesController {
-    @FXML
-    private Button bookings_button;
+    import java.io.IOException;
 
-    @FXML
-    private Button history_button;
+    public class RidesController {
+        @FXML
+        private Button bookings_button;
 
-    @FXML
-    private Button home_button;
+        @FXML
+        private Button history_button;
 
-    @FXML
-    private Button join_button;
+        @FXML
+        private Button home_button;
 
-    @FXML
-    private Button logout_button;
+        @FXML
+        private Button join_button;
 
-    @FXML
-    private Pane pane_111;
+        @FXML
+        private Button logout_button;
 
-    @FXML
-    private Pane pane_1111;
+        @FXML
+        private Pane pane_111;
 
-    @FXML
-    private Pane pane_112;
+        @FXML
+        private Pane pane_1111;
 
-    @FXML
-    private Pane pane_1121;
+        @FXML
+        private Pane pane_112;
 
-    @FXML
-    private Pane pane_131;
+        @FXML
+        private Pane pane_1121;
 
-    @FXML
-    private Button rent_button;
+        @FXML
+        private Pane pane_131;
 
-    @FXML
-    private Button request_button;
+        @FXML
+        private Button rent_button;
 
-    @FXML
-    private Button rides_button;
+        @FXML
+        private Button request_button;
 
-    @FXML
-    private HBox root;
+        @FXML
+        private Button rides_button;
 
-    @FXML
-    private AnchorPane side_ankerpane;
-    private void pageNavigation()
-    {
-        home_button.setOnAction(event -> {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/dashboard/dashboard.fxml"));
-                Parent homeRoot = loader.load();
-                Scene homeScene = new Scene(homeRoot);
-                Stage stage = (Stage) home_button.getScene().getWindow();
-                stage.setScene(homeScene);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        rides_button.setOnAction(event -> {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/rides/rides.fxml"));
-                Parent ridesRoot = loader.load();
-                Scene ridesScene = new Scene(ridesRoot);
-                Stage stage = (Stage) rides_button.getScene().getWindow();
-                stage.setScene(ridesScene);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        rent_button.setOnAction(event -> {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/station/front/station.fxml"));
-                Parent rentRoot = loader.load();
-                Scene rentScene = new Scene(rentRoot);
-                Stage stage = (Stage) rent_button.getScene().getWindow();
-                stage.setScene(rentScene);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        @FXML
+        private HBox root;
 
+        @FXML
+        private AnchorPane side_ankerpane;
+
+        private void pageNavigation() {
+            home_button.setOnAction(event -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/dashboard/dashboard.fxml"));
+                    Parent homeRoot = loader.load();
+                    Scene homeScene = new Scene(homeRoot);
+                    Stage stage = (Stage) home_button.getScene().getWindow();
+                    stage.setScene(homeScene);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            rides_button.setOnAction(event -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/rides/rides.fxml"));
+                    Parent ridesRoot = loader.load();
+                    Scene ridesScene = new Scene(ridesRoot);
+                    Stage stage = (Stage) rides_button.getScene().getWindow();
+                    stage.setScene(ridesScene);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            rent_button.setOnAction(event -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/station/front/station.fxml"));
+                    Parent rentRoot = loader.load();
+                    Scene rentScene = new Scene(rentRoot);
+                    Stage stage = (Stage) rent_button.getScene().getWindow();
+                    stage.setScene(rentScene);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            join_button.setOnAction(event -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/booking/booking.fxml"));
+                    Parent bookingRoot = loader.load();
+                    Scene bookingScene = new Scene(bookingRoot);
+                    Stage stage = (Stage) join_button.getScene().getWindow();
+                    stage.setScene(bookingScene);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        @FXML
+        void initialize() {
+            pageNavigation();
+        }
     }
-    @FXML
-    void initialize() {
-        pageNavigation();
-    }
-}
